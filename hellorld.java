34e9f977bf20212ac51b83bb599b6374a71655f9
wessel21222 <123621194+wessel21222@users.noreply.github.com>
2023-02-02 09:17:51 +0000

    Update hellorld.java

--- hellorld.java
+++ hellorld.java
@@ -1,6 +1,6 @@
 class Hellorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("dat change: 5");
+        System.out.println("dat change: 6");
     }
 }
